7204ece3851a8d49b780ed5119b9862fa1128f84
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-25 18:12:34 -0400

    Add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter, Routes, Route, Outlet, Link,
+} from 'react-router-dom';
+import AddNewBook from './components/AddNewBook';
+import BookCard from './components/BookCard';
+import BookList from './components/BookList';
 import './App.css';
+
+function Layout() {
+  return (
+    <main>
+      <section className="navSection">
+        <h1>Cool Bookstore</h1>
+        <ul className="navstyle">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
+      </section>
+      <Outlet />
+    </main>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<BookList />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<div>If page not found it goes here</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/BookCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BookCard = () => {
+
+  return (
+    Genre,
+    Title,
+    Author,
+    Progress,
+    
+  )
+};
